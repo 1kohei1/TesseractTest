a4c77e2a0ef1a01f49629f4d19e2fddf03de462d
Kohei Arai <tobecomebig@gmail.com>
2014-09-04 15:32:11 -0400

    Initial Commit

---
+++ TesseractTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TesseractTest
+//
+//  Created by Kohei Arai on 9/4/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TesseractTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TesseractTest
+//
+//  Created by Kohei Arai on 9/4/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
